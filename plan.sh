57e3bf555c79310f0b8aa63c4810aa6084bd2154
Nate O'Connel <nateoconnell@gmail.com>
2021-06-28 08:31:41 -0500

    Update binary provider and package version

--- plan.sh
+++ plan.sh
@@ -1,15 +1,16 @@
 pkg_origin=core
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_name=openjdk11
-# NOTE: Retrieve download link and shasum from here: https://jdk.java.net/11/
-pkg_version=11.0.2
-pkg_source=https://download.java.net/java/GA/jdk11/9/GPL/openjdk-${pkg_version}_linux-x64_bin.tar.gz
-pkg_shasum=99be79935354f5c0df1ad293620ea36d13f48ec3ea870c838f20c504c9668b57
-pkg_filename=openjdk-${pkg_version}_linux-x64_bin.tar.gz
-pkg_dirname="jdk-${pkg_version}"
-pkg_license=("GPL-2.0-only")
-pkg_description=('OpenJDK is a free and open-source implementation of the Java Platform, Standard Edition.')
-pkg_upstream_url=https://openjdk.java.net/
+# NOTE: Retrieve download link and shasum from here: https://adoptopenjdk.net/releases.html
+pkg_version=11.0.11
+pkg_patch_lvl=9
+pkg_source="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-${pkg_version}%2B${pkg_patch_lvl}/OpenJDK11U-jdk_x64_linux_hotspot_${pkg_version}_${pkg_patch_lvl}.tar.gz"
+pkg_shasum=e99b98f851541202ab64401594901e583b764e368814320eba442095251e78cb
+pkg_filename="OpenJDK11U-jdk_x64_linux_hotspot_${pkg_version}_${pkg_patch_lvl}.tar.gz"
+pkg_dirname="jdk-${pkg_version}+${pkg_patch_lvl}"
+pkg_license=("GPL-2.0-only WITH Classpath-exception-2.0")
+pkg_description=('OpenJDK is a free and open-source implementation of the Java Platform, Standard Edition. AdoptOpenJDK provides binaries from the upstream OpenJDK source code.')
+pkg_upstream_url=https://adoptopenjdk.net/about.html
 pkg_deps=(
   core/alsa-lib
   core/freetype
@@ -47,7 +48,7 @@ do_install() {
   build_line "Setting rpath for all libraries to '$LD_RUN_PATH'"
 
   find "$pkg_prefix"/bin -type f -executable \
-    -exec sh -c 'file -i "$1" | grep -q "x-executable; charset=binary"' _ {} \; \
+    -exec sh -c 'file -i "$1" | grep -q "x-pie-executable; charset=binary"' _ {} \; \
     -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "${LD_RUN_PATH}" {} \;
 
   find "$pkg_prefix/lib" -type f -name "*.so" \
