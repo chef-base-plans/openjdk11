79a7ab6e2e2e1e1bf6e45db5607f6335c1302957
Nimit <nimit.jyotiana@hotmail.com>
2021-12-22 22:23:01 +0530

    upgraded openjdk11 to v11.0.13+8
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- plan.sh
+++ plan.sh
@@ -1,30 +1,17 @@
 pkg_origin=core
-pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_name=openjdk11
-# NOTE: Retrieve download link and shasum from here: https://jdk.java.net/11/
-pkg_version=11.0.13
-pkg_source=https://github.com/openjdk/jdk11u/archive/refs/tags/jdk-${pkg_version}-ga.tar.gz
-pkg_shasum=e98eb999aa5e85b330e4aa062567743e290dfe925c3fe854d9c6e2c55399da59
-pkg_dirname=jdk11u-jdk-${pkg_version}-ga
-pkg_license=("GPL-2.0-only")
-pkg_description=('OpenJDK is a free and open-source implementation of the Java Platform, Standard Edition.')
-pkg_upstream_url=https://openjdk.java.net/
+pkg_version=11.0.13+8
+pkg_description=('OpenJDK is a free and open-source implementation of the Java Platform, Standard Edition. OpenJDK distribution from Adoptium')
+pkg_license=("GPL-2.0-with-classpath-exception")
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_upstream_url=https://adoptium.net
+pkg_source=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.13%2B8/OpenJDK11U-jdk_x64_linux_hotspot_11.0.13_8.tar.gz
+pkg_shasum=3b1c0c34be4c894e64135a454f2d5aaa4bd10aea04ec2fa0c0efe6bb26528e30
+pkg_dirname=jdk-${pkg_version}-src
 pkg_deps=(
-  core/alsa-lib
-  core/freetype
-  core/gcc-libs
-  core/glibc
-  core/libxext
-  core/libxi
-  core/libxrender
-  core/libxtst
-  core/xlib
-  core/zlib
 )
 pkg_build_deps=(
-	core/patchelf
-       	core/rsync
-	core/autoconf
+	core/rsync
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
@@ -33,36 +20,32 @@ pkg_include_dirs=(include)
 source_dir=$HAB_CACHE_SRC_PATH/${pkg_dirname}
 
 do_setup_environment() {
- set_runtime_env JAVA_HOME "$pkg_prefix"
+	set_runtime_env JAVA_HOME "$pkg_prefix"
 }
 
 do_build() {
-	pushd "${source_dir}" || exit 1
-	bash configure
-	make images
-	./build/*/images/jdk/bin/java --version
-	popd
+	return 0
 }
 
 do_install() {
-  pushd "${pkg_prefix}" || exit 1
-  rsync -avz "${source_dir}/" .
+	pushd "${pkg_prefix}" || exit 1
+	rsync -avz "${source_dir}/" .
 
-  export LD_RUN_PATH="${LD_RUN_PATH}:${pkg_prefix}/lib/jli:${pkg_prefix}/lib/server:${pkg_prefix}/lib"
+	export LD_RUN_PATH="${LD_RUN_PATH}:${pkg_prefix}/lib/jli:${pkg_prefix}/lib/server:${pkg_prefix}/lib"
 
-  build_line "Setting interpreter for all executables to '$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2'"
-  build_line "Setting rpath for all libraries to '$LD_RUN_PATH'"
+	build_line "Setting interpreter for all executables to '$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2'"
+	build_line "Setting rpath for all libraries to '$LD_RUN_PATH'"
 
-  find "$pkg_prefix"/bin -type f -executable \
-    -exec sh -c 'file -i "$1" | grep -q "x-executable; charset=binary"' _ {} \; \
-    -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "${LD_RUN_PATH}" {} \;
+	find "$pkg_prefix"/{lib,bin} -type f -executable \
+	-exec sh -c 'file -i "$1" | grep -q "x-pie-executable; charset=binary"' _ {} \; \
+	-exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" --set-rpath "${LD_RUN_PATH}" {} \;
 
-  find "$pkg_prefix/lib" -type f -name "*.so" \
-    -exec patchelf --set-rpath "${LD_RUN_PATH}" {} \;
+	find "$pkg_prefix/lib" -type f -name "*.so" \
+	-exec patchelf --set-rpath "${LD_RUN_PATH}" {} \;
 
-  popd || exit 1
+	popd || exit 1
 }
 
 do_strip() {
-  return 0
+	return 0
 }
